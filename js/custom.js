ee92d7796fd91f66fab6c3d75d49bbc416e47c75
Lucid <67615874+Lucid-017@users.noreply.github.com>
2022-07-25 21:44:56 +0100

    resume update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
-            var fullImgBox= document.getElementById('fullImgBox');
+console.log(this)
+          var fullImgBox= document.getElementById('fullImgBox');
             var fullImg= document.getElementById('fullImg');
             console.log(fullImg)
 
@@ -9,3 +10,40 @@
             function closeFullImg(){
                 fullImgBox.style.transform = 'scale(0)'
             }
+            $(document).ready(function () {
+        
+
+                const filterContainer = document.querySelector(".gallery-filter"),
+                galleryItems = document.querySelectorAll(".gallery-item");
+        
+                filterContainer.addEventListener("click", (event) =>{
+                  if(event.target.classList.contains("filter-item")){
+                    // deactivate existing active 'filter-item'
+                    filterContainer.querySelector(".active").classList.remove("active");
+                    // activate new 'filter-item'
+                    event.target.classList.add("active");
+                    const filterValue = event.target.getAttribute("data-filter");
+                    galleryItems.forEach((item) =>{
+                      if(item.classList.contains(filterValue) || filterValue === 'all'){
+                        item.classList.remove("hide");
+                        item.classList.add("show");
+                      }
+                      else{
+                        item.classList.remove("show");
+                        item.classList.add("hide");
+                      }
+                    });
+                  }
+                });
+              
+                //nav icon animation
+                $(".first-button").on("click", function () {
+                  $(".animated-icon1").toggleClass("open");
+                });
+                $(".second-button").on("click", function () {
+                  $(".animated-icon2").toggleClass("open");
+                });
+                $(".third-button").on("click", function () {
+                  $(".animated-icon3").toggleClass("open");
+                });
+              });
